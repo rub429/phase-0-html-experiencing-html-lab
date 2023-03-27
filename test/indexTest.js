b7bbd77f5c46891666d18a87b89c63d5508f0d93
Ruben <rub429@gmail.com>
2023-03-27 12:59:11 -0400

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,13 +2,13 @@ const { expect } = require("chai");
 const fs = require("fs");
 const path = require("path");
 
-describe("my_list.html", () => {
+describe("myList.html", () => {
   it("should be present", () => {
     const fileExists = fs.existsSync(
-      path.resolve(__dirname, "..", "my_list.html")
+      path.resolve(__dirname, "..", "myList.html")
     );
     const errorHint =
-      "Make sure you created a file called 'my_list.html' in the lab directory with your HTML document in it!";
+      "Make sure you created a file called 'myList.html' in the lab directory with your HTML document in it!";
 
     expect(fileExists, errorHint).to.be.true;
   });
